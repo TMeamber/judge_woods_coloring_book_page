52b7a791e4045d525ec8812395487183902a9e93
Travis Meamber <tmeamber623@gmail.com>
2018-02-26 17:53:11 -0800

    added dependencies and some p tags

--- coloring-book/src/App.js
+++ coloring-book/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <p> Vote </p>
+        <p> Judge </p>
+        <p> Woods </p>
       </div>
     );
   }
